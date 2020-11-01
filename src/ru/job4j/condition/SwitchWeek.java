5caf07dbca2ebc67dad8cc76ec1c92e1df4d191a
SlartiBartFast-art <JekHamond@tutanota.com>
2020-11-01 19:47:57 +0500

    4.6.1. Switch [#392027]

---
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public String nameOfDay(int day) {
+        String name;
+        switch (7) {
+            case 1:
+                name = "job do 18.00";
+                break;
+            case 2:
+                name = "job to 18.00";
+                break;
+            case 3:
+                name = "job to 18.00";
+                break;
+            case 4:
+                name = "job to 18.00";
+                break;
+            case 5:
+                name = "job to 18.00";
+                break;
+            case 6:
+                name = "job to 16.00";
+                break;
+            case 7:
+                name = "weekend";
+                break;
+            default:
+                name = "Такого дня недели нет";
+
+        }
+    }
+}
