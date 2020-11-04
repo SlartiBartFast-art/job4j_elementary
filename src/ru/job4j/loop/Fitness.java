40671a5bd385cf54e18b01809025d1956545ccea
SlartiBartFast-art <JekHamond@tutanota.com>
2020-11-04 21:14:19 +0500

    5.4. Протеиновая диета [#392128]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,8 +3,9 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan *= 3;
+            nik *= 2;
             month++;
             System.out.println(month);
         }
