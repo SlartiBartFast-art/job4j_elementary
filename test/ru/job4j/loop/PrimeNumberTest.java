6b5f9597f3f3eccd717c213e7d4d27b64ac5d839
SlartiBartFast-art <JekHamond@tutanota.com>
2020-11-04 21:43:51 +0500

    5.6. Простые числа [#392129]@Test

---
+++ test/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class PrimeNumberTest {
+
+    @Test
+    public void whenNumbercalcToFinish() {
+        int rsl = PrimeNumber.calc(5);
+        assertThat(rsl, is(3));
+    }
+
+    @Test
+    public void when11() {
+        int count = PrimeNumber.calc(11);
+        assertThat(count, is(5));
+    }
+
+    @Test
+    public void when2() {
+        int count = PrimeNumber.calc(2);
+        assertThat(count, is(1));
+    }
+}
