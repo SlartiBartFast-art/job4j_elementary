adb0f1e6061443591091868dbbf456a0023cb540
SlartiBartFast-art <JekHamond@tutanota.com>
2020-11-08 17:14:28 +0500

    6.0.2. Заполнение массива. [#392052]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,10 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surname.length);
         float[] price = new float[40];
         System.out.println("Размер массива равен: " + price.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Petr Petrov";
+        names[2] = "Semen Sidorov";
+        names[3] = "Ivan Petrov";
     }
 }
