030d0c1c168b50e538ba4babf3723baaeb19c4d6
SlartiBartFast-art <JekHamond@tutanota.com>
2020-11-19 15:08:52 +0500

    6.9. Жадный алгоритм. Сдача в кофе машине

---
+++ src/ru/job4j/sort/Machine.java
@@ -0,0 +1,27 @@
+package ru.job4j.sort;
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] coins = {10, 5, 2, 1};
+
+   public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int index = 0;
+        int change = money - price;
+
+      for (int i = 0; i < coins.length; i++) {
+          while (change >= coins[i]) {
+               change = change - coins[i];
+               rsl[index] = coins[i];
+               size = index + 1;
+               index++;
+           }
+      }
+       return Arrays.copyOf(rsl, size);
+     }
+
+
+    }
+
+
