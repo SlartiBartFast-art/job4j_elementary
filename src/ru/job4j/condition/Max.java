ae5f243fe4442422fd11d372e4cb413d0b51af1d
SlartiBartFast-art <JekHamond@tutanota.com>
2020-11-30 18:50:24 +0500

    3. Перегрузить метод max для трех чисел [#392230]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,9 +1,18 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
+    public static int max(int first, int second) {
+        int result = first > second ? first : second;
         return result;
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(first, max(second, third, fourth));
     }
 
     public static void main(String[] args) {
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -23,4 +23,24 @@ public class MaxTest {
         int result = Max.max(14, 14);
         assertThat(result, is(14));
     }
+    @Test
+    public void when25To14and19Then25() {
+        int result  = Max.max(25, 14, 19);
+        assertThat(result, is(25));
+    }
+    @Test
+    public void when19To25and14Then25() {
+        int result = Max.max(19, 25, 14);
+        assertThat(result, is(25));
+    }
+    @Test
+    public void  when19To15and17and18Then19() {
+        int result = Max.max(19, 15, 17, 18);
+        assertThat(result, is(19));
+    }
+    @Test
+    public void when14To15and17and18Then18() {
+        int result  = Max.max(14, 15, 17, 18);
+        assertThat(result, is(18));
+    }
 }
