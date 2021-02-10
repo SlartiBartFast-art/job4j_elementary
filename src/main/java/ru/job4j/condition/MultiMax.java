c4dc579b27901e07cb258d5cbaf09a01d64a0db4
SlartiBartFast-art <JekHamond@tutanota.com>
2021-02-10 17:05:51 +0500

    0. Настройка окружения для Maven. [# 471684]

--- src/main/java/ru/job4j/condition/MultiMax.java
+++ src/main/java/ru/job4j/condition/MultiMax.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int first, int second, int third) {
-        int i = first > second ? first : third;
+        int i = first > second ? first : second;
         int result = i > third ? i : third;
         return result;
     }
